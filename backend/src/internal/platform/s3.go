216d08210ace33223d881d9be810499200c39688
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-06-05 21:37:13 +0100

    feat(backend/platform): add SaveToS3 method

--- backend/src/internal/platform/s3.go
+++ backend/src/internal/platform/s3.go
@@ -2,11 +2,15 @@ package platform
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/transfermanager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/google/uuid"
 )
 
 type S3Client struct {
@@ -41,4 +45,28 @@ func NewS3Client() (S3Client, error) {
 	return S3Client{
 		Client: client,
 	}, nil
+}
+
+func (s3 *S3Client) SaveToS3(ctx context.Context, bucket string, reader io.Reader, ownerID uuid.UUID, fileName string) error {
+	key := fmt.Sprintf("%s/%s", ownerID, fileName)
+
+	var partMiBs int64 = 5
+
+	uploader := transfermanager.New(s3.Client, func(opts *transfermanager.Options) {
+		opts.PartSizeBytes = partMiBs * 1024 * 1024
+		opts.Concurrency = 3
+	})
+
+	_, err := uploader.UploadObject(ctx, &transfermanager.UploadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   reader,
+	})
+	if err != nil {
+		log.Printf("s3 transfer manager put error: %v", err)
+		return err
+	}
+
+	return nil
+
 }
