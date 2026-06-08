2139875f813c33ce740e0b62dc55ee2cc8ee8421
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-06-08 09:15:18 +0100

    feat(backend/provider): add Provider struct
    Will eventually contain wrapper interfaces for supported cloud providers. Currently, supports only AWS S3.

--- backend/src/internal/platform/cloud_provider.go
+++ backend/src/internal/platform/cloud_provider.go
@@ -1,1 +1,10 @@
 package platform
+
+import "github.com/aws/aws-sdk-go-v2/service/s3"
+
+type Provider struct {
+	ObjectStorageClient *s3.Client
+}
+
+// TODO: build wrapper for each provider.
+type ObjectStorageProvider interface{}
