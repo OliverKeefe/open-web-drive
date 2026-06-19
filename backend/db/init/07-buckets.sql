feeee0da3e6026b9660282508914015642398b79
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-06-19 14:27:56 +0100

    feat(backend/db): add buckets script
    * add storage_bucket table
    * add storage_bucket_groups table

---
+++ backend/db/init/07-buckets.sql
@@ -0,0 +1,15 @@
+-- BUCKETS
+CREATE TABLE storage_bucket (
+    id UUID PRIMARY KEY NOT NULL,
+    org_id UUID NOT NULL REFERENCES orgs(id) ON DELETE CASCADE,
+    name VARCHAR(64) NOT NULL,
+    groups NOT NULL REFERENCES storage_bucket_groups,
+    owner UUID NOT NULL REFERENCES users(id)
+);
+
+-- BUCKET GROUP MEMBERSHIP
+CREATE TABLE storage_bucket_groups (
+    bucket_id UUID NOT NULL REFERENCES bucket(id) ON DELETE CASCADE,
+    group_id UUID NOT NULL REFERENCES groups(id) ON DELETE CASCADE,
+    PRIMARY KEY (bucket_id, group_id)
+);
