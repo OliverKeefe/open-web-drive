81df3fd48e63bbc355530a097730d1136c9f305a
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-06-19 14:26:36 +0100

    chore(backend/db): add spacing
    * add files table
    * add file_permissions table
    * add file_groups table
    * add metadata table
    * add file_metadata table
    * add file_versions table
    * add file_metadata_access table
    * add file_metadata_group_access table
    * add file_user_access table
    * create idx_file_user_modified_desc index for pagination

--- backend/db/init/05-files.sql
+++ backend/db/init/05-files.sql
@@ -4,20 +4,20 @@ CREATE TABLE files (
     permissions_id REFERENCES permissions(id)
 );
 
-CREATE TABLE file_permissions(
+CREATE TABLE file_permissions (
     file_id REFERENCES files(id) NOT NULL,
     permissions_id REFERENCES permissions(id) NOT NULL,
     PRIMARY KEY (file_id, permissions_id)
 );
 
-CREATE TABLE file_groups(
+CREATE TABLE file_groups (
     file_id REFERENCES files(id) ON DELETE CASCADE,
     group_id REFERENCES groups(id) ON DELETE CASCADE,
     PRIMARY KEY (file_id, group_id)
 );
 
 -- METADATA
-CREATE TABLE metadata(
+CREATE TABLE metadata (
     id UUID PRIMARY KEY NOT NULL DELETE CASCADE,
     owner UUID REFERENCES users(id) NOT NULL,
     file_name VARCHAR(255) NOT NULL DELETE CASCADE,
@@ -33,33 +33,33 @@ CREATE TABLE metadata(
 );
 
 -- FILE <-> METADATA
-CREATE TABLE file_metadata(
+CREATE TABLE file_metadata (
     file_id UUID NOT NULL REFERENCES files(id) ON DELETE CASCADE,
     metadata_id UUID NOT NULL REFERENCES metadata(id) ON DELETE CASCADE,
     CONSTRAINT pk_file_metadata PRIMARY KEY (file_id, metadata_id)
 );
 
 -- FILE VERSIONS
-CREATE TABLE file_versions(
+CREATE TABLE file_versions (
     version_id UUID REFERENCES file_metadata(id) NOT NULL,
     file_id UUID REFERENCES files(id) NOT NULL,
     CONSTRAINT pk_file_version PRIMARY KEY (file_id, version_id)
 );
 
 -- FILE <-> USER ACCESS (MtM)
-CREATE TABLE file_metadata_access(
+CREATE TABLE file_metadata_access (
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     file_id UUID NOT NULL REFERENCES file_metadata(id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, file_id)
 );
 
 -- FILE <->  GROUP ACCESS (MtM)
-CREATE TABLE file_metadata_group_access(
+CREATE TABLE file_metadata_group_access (
     file_id UUID REFERENCES file_metadata(id) ON DELETE CASCADE,
     group_id UUID REFERENCES groups(id) ON DELETE CASCADE
 );
 
-CREATE TABLE file_user_access(
+CREATE TABLE file_user_access (
     file_id UUID REFERENCES file_metadata(id) ON DELETE CASCADE,
     user_id UUID REFERENCES users(id) ON DELETE CASCADE
 );
