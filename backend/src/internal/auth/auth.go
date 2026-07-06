d6977e152a2e472b0391ef92596d64693e390b71
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-06 17:59:17 +0100

    feat(backend/auth): add HasClaim function
    * add HasClaim function
    
    HasClaim checks the user possesses the correct permission scope claims in JWT stored in context.

--- backend/src/internal/auth/auth.go
+++ backend/src/internal/auth/auth.go
@@ -2,31 +2,11 @@ package auth
 
 import (
 	"context"
+	"errors"
+	"strings"
 
-	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
-
-type JWTRequest struct {
-	Alg    string `json:"alg"`
-	Typ    string `json:"typ"`
-	Sub    string `json:"sub"`
-	Claims string `json:"name"`
-	Admin  string `json:"admin"`
-	Iat    string `json:"iat"`
-}
-
-type Claims struct {
-	Claims interface{}
-}
-
-type OAuth2 interface {
-	Authenticator(ctx context.Context, rawJWT string) (*Claims, error)
-	ReissueJWT(ctx context.Context, refreshToken string) (string, error)
-}
-
-type UCAN interface {
-	ValidateJWT(token jwt.Token) (bool, error)
-}
 
 func UserIDFromCtx(ctx context.Context) (string, bool) {
 	userID, ok := ctx.Value(userIDKey).(string)
@@ -36,4 +16,44 @@ func UserIDFromCtx(ctx context.Context) (string, bool) {
 func ClaimsFromCtx(ctx context.Context) (map[string]interface{}, bool) {
 	claims, ok := ctx.Value(claimsKey).(map[string]interface{})
 	return claims, ok
+}
+
+func HasClaim(ctx context.Context, userID uuid.UUID, requiredClaim string) (bool, error) {
+	userIDStoredInCtx, ok := UserIDFromCtx(ctx)
+	if !ok {
+		return false, errors.New("unauthenticated: no user ID found in context")
+	}
+
+	if userIDStoredInCtx != userID.String() {
+		return false, errors.New("unauthorized: userID mismatch")
+	}
+
+	claims, ok := ClaimsFromCtx(ctx)
+	if !ok {
+		return false, errors.New("unable to get claims from context")
+	}
+
+	scopes, present := claims["scopes"]
+	if !present {
+		return false, errors.New("no scope claims found in token")
+	}
+
+	scopesStr, ok := scopes.(string)
+	if !ok {
+		return false, errors.New("couldn't convert scope claim to string")
+	}
+
+	hasClaim := false
+	for _, scope := range strings.Fields(scopesStr) {
+		if scope == requiredClaim {
+			hasClaim = true
+			break
+		}
+	}
+
+	if !hasClaim {
+		return false, errors.New("unauthorized: missing required scope")
+	}
+
+	return true, nil
 }
