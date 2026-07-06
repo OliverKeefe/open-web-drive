ff0a7e1721752c08b515e049736135916c9e732c
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-06 18:38:39 +0100

    refactor(backend/files): decompose search into vertical slice

---
+++ backend/src/internal/service/files/search.go
@@ -0,0 +1,81 @@
+package files
+
+import (
+	"backend/src/internal/api/message"
+	"backend/src/internal/platform"
+	"context"
+	"log"
+	"net/http"
+)
+
+type SearchService struct {
+	db                DB
+	BlobStorageClient *platform.BlobStorageClient
+	BucketURL         string
+}
+
+func (svc *SearchService) Handle(w http.ResponseWriter, r *http.Request) {
+
+	if r.Body == nil || r.ContentLength <= 0 {
+		http.Error(w, "invalid request", http.StatusBadRequest)
+		return
+	}
+
+	var request GetAllMetadataRequest
+	err := request.Bind(r)
+	if err != nil {
+		log.Printf("couldn't map http request to dto: %v", err)
+		http.Error(w, "invalid request", http.StatusBadRequest)
+		return
+	}
+
+	files, err := svc.execute(r.Context(), request)
+	if err != nil {
+		log.Printf("couldn't get all user's files: %v", err)
+		http.Error(w, "unable to get user's files", http.StatusInternalServerError)
+		return
+	}
+
+	if err = message.Response(w, "fetched user's files", files); err != nil {
+		log.Printf("unable to return response, %v", err)
+		return
+	}
+}
+
+func (svc *SearchService) execute(ctx context.Context, request GetAllMetadataRequest) ([]MetaDataResponse, error) {
+	var (
+		repo     = svc.db
+		files    []MetaData
+		response []MetaDataResponse
+	)
+
+	files, err := repo.FindAllMetadata(ctx, request)
+	if err != nil {
+		log.Printf("unable to get all files for user: %s, %v ", request.UserID, err)
+	}
+
+	for _, file := range files {
+		file := file.ToResponse()
+		if err != nil {
+			log.Printf("unable to map file metadata: %v, to dto: %v", file, err)
+		}
+		response = append(response, file)
+	}
+
+	return response, nil
+}
+
+func (svc *SearchService) findMetadata(ctx context.Context, request FindMetadataRequest) ([]MetaData, error) {
+	var (
+		files []MetaData
+	)
+
+	model := request.ToModel()
+	files, err := svc.db.FindMetadata(ctx, model)
+	if err != nil {
+		log.Printf("unable to get file metadata: %v", err)
+		return files, err
+	}
+
+	return files, nil
+}
