edc46803022f9ef6ede2014e084ea174d8b1aba9
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-07 22:20:17 +0100

    feat(backend/files): add update service scaffolding
    * add updateMetadataRepository interface
    * add UpdateMetadataService struct
    * add NewUpdateMetadataService constructor scaffold
    * add Handle method scaffold
    * add updateMetadataRequest request struct scaffold
    * add execute method scaffold
    
    UpdateMetadataService is the usecase for changing file metadata e.g. renaming or moving directory location / path.

---
+++ backend/src/internal/service/files/update.go
@@ -0,0 +1,38 @@
+package files
+
+import (
+	"backend/src/internal/platform"
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type updateMetadataRepository interface {
+	CheckExists(ctx context.Context, ID uuid.UUID) (bool, error)
+}
+
+type UpdateMetadataService struct {
+	Db                updateMetadataRepository
+	BlobStorageClient *platform.BlobStorageClient
+	BucketURL         string
+}
+
+func NewUpdateMetadataService(db updateMetadataRepository, client *platform.BlobStorageClient, bucketUrl string) *UpdateMetadataService {
+	return &UpdateMetadataService{
+		Db:                db,
+		BlobStorageClient: client,
+		BucketURL:         bucketUrl,
+	}
+}
+
+func (svc *UpdateMetadataService) Handle(w http.ResponseWriter, r *http.Request) {
+	panic("not implemented")
+}
+
+type updateMetadataRequest struct {
+}
+
+func (svc *UpdateMetadataService) execute(req updateMetadataRequest) error {
+	panic("not implemented")
+}
