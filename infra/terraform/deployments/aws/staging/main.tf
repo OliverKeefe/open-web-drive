fcfb01e4b49e8259de11691ea40488ef5856084c
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-10 16:27:52 +0100

    ops(aws/storage): add storage backend modules

--- infra/terraform/deployments/aws/staging/main.tf
+++ infra/terraform/deployments/aws/staging/main.tf
@@ -13,3 +13,25 @@ provider "aws" {
   region = aws_region
 }
 
+# Will deploy s3 by default unless specified.
+module "s3_storage" {
+  count = var.storage_backend == "s3" ? 1 : 0
+  source = "../../../modules/aws/storage"
+
+  bucket_name = "gestalt-staging-s3-bucket"
+  environment = "staging"
+
+  tags = {
+    Vendor    = "AWS"
+    ManagedBy = "Terraform"
+    Project   = "GestaltApp"
+  }
+}
+
+module "minio_storage" {
+  count = var.storage_backend == "minio" ? 1 : 0
+  source = "../../../modules/container/"
+
+  environment = "staging"
+}
+
