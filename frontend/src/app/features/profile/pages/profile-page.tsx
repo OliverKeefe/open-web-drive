2f49c70077e5b724fef5407077c1e2ee874c0838
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-22 18:16:12 +0100

    chore(frontend): add placeholder pages

---
+++ frontend/src/app/features/profile/pages/profile-page.tsx
@@ -0,0 +1,7 @@
+export default function ProfilePage() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <p>Profile</p>
+    </div>
+  );
+}
---
+++ frontend/src/app/features/storage/pages/hot-storage-page.tsx
@@ -0,0 +1,7 @@
+export default function HotStoragePage() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <p>Hot Storage Settings</p>
+    </div>
+  );
+}
