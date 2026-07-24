2f2a9ab421a533916ef0da80d8723afe8820378a
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-24 18:51:31 +0100

    chore(backend/db): add todo comment

--- backend/db/init/01-extensions.sql
+++ backend/db/init/01-extensions.sql
@@ -1,1 +1,4 @@
+-- TODO: create a custom dockerfile image and install pg_uuidv7 to handle
+-- uuidv7 generation at the database level instead of the application layer.
+-- CREATE EXTENSION IF NOT EXISTS "pg_uuidv7";
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
