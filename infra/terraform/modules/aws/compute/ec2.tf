0a60143d25859011eb69e8c6c39c35c1ac79294c
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-27 22:32:07 +0100

    ops: add project_name and common_tags outputs
    - Added outputs for shared/local.

--- infra/terraform/modules/aws/compute/ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "app_server" {
-  ami = ""
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "StagingInstance"
-  }
-}
---
+++ infra/terraform/shared/outputs.tf
@@ -0,0 +1,8 @@
+
+output "project_name" {
+  value = local.project_name
+}
+
+output "common_tags" {
+  value = local.common_tags
+}
