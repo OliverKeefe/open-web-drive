070ac58578b27694b01361d846ff995a9f937730
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-28 20:21:09 +0100

    ops(aws/staging): add main deployment file for aws staging
    - Import networking module.
    - Import shared module.
    - Set provider region, endpoints to localstack url and skip checks (required for localstack)
    - Set required terraform version to 1.15.8
    - Set source hashicorp/aws, and version 5.0.
    - Remove minio storage module import.
    - Remove aws instance resource declaration.
    - Remove s3_storage module import.

--- infra/terraform/deployments/aws/staging/main.tf
+++ infra/terraform/deployments/aws/staging/main.tf
@@ -1,42 +1,26 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.15.8"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = aws_region
+  region = var.aws_region
 }
 
-# Will deploy s3 by default unless specified.
-module "s3_storage" {
-  count = var.storage_backend == "s3" ? 1 : 0
-  source = "../../../modules/aws/storage"
-
-  bucket_name = "gestalt-staging-s3-bucket"
-  environment = "staging"
-
-  tags = {
-    Vendor    = "AWS"
-    ManagedBy = "Terraform"
-    Project   = "GestaltApp"
-  }
+module "shared" {
+  source = "../../../shared"
 }
 
-module "minio_storage" {
-  count = var.storage_backend == "minio" ? 1 : 0
-  source = "../../../modules/container/"
-
-  environment = "staging"
-}
-
-resource "aws_instance" "aws_instance_name" {
-  tags = {
-    Name = aws_instance_name
-  }
+module "networking" {
+  source = "../../../modules/aws/networking"
+  vpc_cidr = var.vpc_cidr
+  environment = var.environment
+  project_name = module.shared.project_name
+  tags = module.shared.common_tags
 }
