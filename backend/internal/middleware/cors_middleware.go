93e0e57282deed5f6055be8457987bb51ae7d752
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-29 18:48:54 +0100

    feat(backend/cors): add env vars
    - Added CORS_ALLOWED_ORIGINS environment variable and check

--- backend/internal/middleware/cors_middleware.go
+++ backend/internal/middleware/cors_middleware.go
@@ -2,11 +2,16 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 )
 
 func EnableCORS(next http.Handler) http.Handler {
+	origin := os.Getenv("CORS_ALLOWED_ORIGINS")
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		if origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
