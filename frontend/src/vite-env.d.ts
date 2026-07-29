42e3d287abc4f31a4a59f4cfa3f722075f2c07d3
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-29 18:57:25 +0100

    feat(frontend/config): add meta interface
    - Added VITE_MAX_UPLOAD_SIZE_BYTES variable to dropzone component.
    
    Interface for environment variable import contract.

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_KEYCLOAK_URL: string;
+    readonly VITE_KEYCLOAK_REALM: string;
+    readonly VITE_KEYCLOAK_CLIENT_ID: string;
+    readonly VITE_BACKEND_BASE_URL: string;
+    readonly VITE_IPFS_GATEWAY_URL: string;
+    readonly VITE_MAX_UPLOAD_SIZE_BYTES: string;
+    readonly VITE_STORACHA_DID: string;
+    readonly VITE_STORACHA_ACCOUNT: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
