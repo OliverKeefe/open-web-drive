1160d074f0b1c3545210478e5099ab91852c90eb
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-07-31 20:07:14 +0100

    feat(frontend/auth): add logout button and handler
    async function calls keycloak.logout() and clears AuthStore of JWT.

--- frontend/src/app/features/shared/components/navigation/sidebars/secondary-sidebar.tsx
+++ frontend/src/app/features/shared/components/navigation/sidebars/secondary-sidebar.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@/components/ui/button"
-import {Flame, FolderClosed, GitBranch, Settings2, Snowflake, SquareTerminal, Users} from "lucide-react"
+import {Flame, FolderClosed, GitBranch, LogOut, Settings2, Snowflake, SquareTerminal, Users} from "lucide-react"
 import React from "react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {Link} from "@tanstack/react-router";
+import {useAuthStore} from "@/security/auth/authstore/auth-store.ts";
+import {logout} from "@/security/auth/keycloak/keycloak.ts";
 
 interface SecondarySidebarProps {
     children?: React.ReactNode
 }
 
 export function SecondarySidebar({ children }: SecondarySidebarProps) {
+    const handleLogout = async() => {
+        useAuthStore.getState().setToken(null);
+        useAuthStore.getState().setUserId(null);
+        await logout();
+    }
+
     return (
         <aside className="fixed left-0 top-0 z-30 h-screen w-16 border-r bg-sidebar flex flex-col">
             <div className="h-12" />
@@ -19,17 +27,9 @@ export function SecondarySidebar({ children }: SecondarySidebarProps) {
                         <FolderClosed />
                     </Button>
                 </Link>
-                <Link to={"/git"}>
-                    <Button variant="ghost" className="cursor-pointer">
-                            <GitBranch />
-                    </Button>
-                </Link>
                 <Button variant="ghost" className="cursor-pointer">
-                    <SquareTerminal />
-                </Button>
-                <Button variant="ghost" className="cursor-pointer">
-                    <Link to={"/hot-storage-settings"}>
-                        <Flame className={"h-8 w-8"}/>
+                    <Link to={"/archive"}>
+                        <Users className={"h-8 w-8"}/>
                     </Link>
                 </Button>
                 <Button variant="ghost" className="cursor-pointer">
@@ -37,15 +37,8 @@ export function SecondarySidebar({ children }: SecondarySidebarProps) {
                         <Snowflake className={"h-8 w-8"}/>
                     </Link>
                 </Button>
-                <Button variant="ghost" className="cursor-pointer">
-                    <Link to={"/archive"}>
-                        <Users className={"h-8 w-8"}/>
-                    </Link>
-                </Button>
-                <Button variant="ghost" className="cursor-pointer">
-                    <Link to={"/settings"}>
-                        <Settings2 className={"h-8 w-8"}/>
-                    </Link>
+                <Button variant="ghost" className="cursor-pointer" onClick={handleLogout}>
+                        <LogOut className={"h-8 w-8"}/>
                 </Button>
                 {children}
             </div>
@@ -60,3 +53,4 @@ export function SecondarySidebar({ children }: SecondarySidebarProps) {
         </aside>
     )
 }
+
