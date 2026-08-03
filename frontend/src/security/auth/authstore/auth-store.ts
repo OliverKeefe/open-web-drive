4d4bfdb654e32d5c6191db6d416a60ea33b942df
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-08-03 14:30:56 +0100

    feat(frontend/auth): get avatar fallback from jwt preferred_username

--- frontend/src/security/auth/authstore/auth-store.ts
+++ frontend/src/security/auth/authstore/auth-store.ts
@@ -3,13 +3,17 @@ import { create } from 'zustand'
 interface AuthState {
     userId: string | null;
     token: string | null;
+    username: string | null;
     setUserId: (id: string | null) => void;
     setToken: (token: string | null) => void;
+    setUsername: (username: string | null) => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
     userId: null,
     token: null,
+    username: null,
     setUserId: (id) => set({ userId: id }),
     setToken: (token) => set({ token: token }),
+    setUsername: (username) => set({ username })
 }));
