2f53f952a50237e2398b34dfc92652d952ef9ec7
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-08-03 14:42:17 +0100

    chore(frontend/auth): fix ide auto-refactor typo
    Changed `build` dir to `bin` for compiled binaries and Goland automatically renamed every build to bin.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,14 @@
     import { createRoot } from 'react-dom/client'
     import './index.css'
     import App from './App.tsx'
-    import { initKeycloak } from '@/security/auth/keycloak/keycloak.ts';
+    import keycloak, { initKeycloak } from '@/security/auth/keycloak/keycloak.ts';
     import { useAuthStore } from "@/security/auth/authstore/auth-store.ts";
 
     const root = createRoot(document.getElementById('root')!);
 
     async function bootstrap() {
         const kc = await initKeycloak();
+        const kcProfile = await keycloak.loadUserProfile();
 
         // If Keycloak determines user not signed in, redirect to login page.
         // The spinner in index.html stays visible until the browser actually leaves the page.
@@ -19,6 +20,7 @@
 
         useAuthStore.getState().setToken(kc.token ?? null);
         useAuthStore.getState().setUserId(kc.tokenParsed?.sub ?? null);
+        useAuthStore.getState().setUsername(kcProfile.username ?? null);
 
         root.render(
             <StrictMode>
