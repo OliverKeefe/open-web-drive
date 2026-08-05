1a02e2d52b71e426b740f18996452c5a4d09215c
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-08-05 22:37:18 +0100

    feat(frontend/routes): rename route paths

--- frontend/src/routes/app-routes.tsx
+++ frontend/src/routes/app-routes.tsx
@@ -5,14 +5,12 @@ import {
 } from "@tanstack/react-router";
 import Login from "@/app/features/auth/pages/login-page";
 import { Files } from "@/app/features/files/pages/files-page";
-import Documents from "@/app/features/documents/pages/documents-page.tsx";
-import Photos from "@/app/features/photos/pages/photos-page";
 import Settings from "@/app/features/settings/pages/settings-page";
-import GitHome from "@/app/features/git/pages/git-home-page";
 import HotStoragePage from "@/app/features/storage/pages/hot-storage-page";
 import ArchivePage from "@/app/features/archive/pages/archive-page";
 import ProfilePage from "@/app/features/profile/pages/profile-page";
 import Layout from "@/app/features/shared/components/layout/layout";
+import VersionControlPage from "@/app/features/versions/pages/version-control-page";
 
 const rootRoute = createRootRoute({
   component: Layout,
@@ -36,28 +34,16 @@ const filesRoute = createRoute({
   component: Files,
 });
 
-const documentsRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/documents",
-  component: Documents,
-});
-
-const photosRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/photos",
-  component: Photos,
-});
-
 const settingsRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/settings",
   component: Settings,
 });
 
-const gitRoute = createRoute({
+const versionControlRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: "/git",
-  component: GitHome,
+  path: "/version-control",
+  component: VersionControlPage,
 });
 
 const hotStorageRoute = createRoute({
@@ -82,10 +68,8 @@ const routeTree = rootRoute.addChildren([
   indexRoute,
   loginRoute,
   filesRoute,
-  documentsRoute,
-  photosRoute,
   settingsRoute,
-  gitRoute,
+  versionControlRoute,
   hotStorageRoute,
   archiveRoute,
   profileRoute,
