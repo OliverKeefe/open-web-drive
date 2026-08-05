1644c7dca773d29b68b24ddf62158c49f91e6cc8
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-08-05 22:37:39 +0100

    feat(frontend/routes): add TooltipProvider to App function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "@/routes/app-routes";
+import {TooltipProvider} from "@/components/ui/tooltip.tsx";
 
 interface AppProps {
     isAuthenticated: boolean;
@@ -10,7 +11,9 @@ interface AppProps {
 function App({ isAuthenticated }: AppProps) {
     return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <RouterProvider router={router} />
+            <TooltipProvider>
+                <RouterProvider router={router} />
+            </TooltipProvider>
         </ThemeProvider>
     );
 }
