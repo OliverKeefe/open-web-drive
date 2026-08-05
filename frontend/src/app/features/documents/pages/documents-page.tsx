90b3fa200ebfd965ec20049d70d7fa0373969af1
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-08-05 22:35:44 +0100

    chore(frontend): remove dead code

--- frontend/src/app/features/documents/pages/documents-page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Documents() {
-    return (
-        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-            <div className="w-full max-w-sm">
-                <p>Documents Page Route Success</p>
-            </div>
-        </div>
-    )
-}
