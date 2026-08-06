bf56f3cf987908a95929496076110307c2d593e1
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-08-06 15:49:41 +0100

    refactor(backend/message): abstract request struct binding
    - Added Bind generic func
    
    Originally, each DTO struct had a Bind for request DTO however, it's just cleaner to abstract this to a generic function and reduce code.

--- backend/internal/api/message/request.go
+++ backend/internal/api/message/request.go
@@ -1,1 +1,15 @@
 package message
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func Bind[T any](r *http.Request) (T, error) {
+	var request T
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return request, fmt.Errorf("invalid json request: %w", err)
+	}
+	return request, nil
+}
